fa0dfcc8818aae05e6651539b44529b2f1fef138
Katalina <Katalina@MacBook-Pro-de-Katalina.local>
2017-08-10 10:52:06 +0100

    rajout style pourcentage au scroll

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const bar = document.querySelector('.bar');
 
 addEventListener("scroll", function (){
+
+  const max = document.body.scrollHeight - innerHeight;
+
+  const pourcentage = pageYOffset / max * 100;
+
+  bar.style.width = pourcentage + "%"; 
+
   console.log(`
     Taille page: ${document.body.scrollHeight}
     Taille affichage: ${innerHeight}
