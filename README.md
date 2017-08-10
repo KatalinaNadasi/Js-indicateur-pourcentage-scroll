1596518e07d48e81b6be1843bbe61bc455bf292d
Katalina <Katalina@MacBook-Pro-de-Katalina.local>
2017-08-10 11:12:32 +0100

    rajout du focus et blur sur un input

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Js-indicateur-pourcentage-scroll
-Indicateur du pourcentage du scroll dans la page
+-Indicateur du pourcentage du scroll dans la page
+-Focus et blur sur un input : rajout du blur quand on clique sur l'input.
--- js/script.js
+++ js/script.js
@@ -6,11 +6,23 @@ addEventListener("scroll", function (){
 
   const pourcentage = pageYOffset / max * 100;
 
-  bar.style.width = pourcentage + "%"; 
+  bar.style.width = pourcentage + "%";
 
   console.log(`
     Taille page: ${document.body.scrollHeight}
     Taille affichage: ${innerHeight}
     Position scroll: ${pageYOffset}
     `);
+});
+
+
+// le focus et blur
+
+const input = document.querySelector("input");
+input.addEventListener("focus", function(){
+  input.style.background = "black";
+});
+
+input.addEventListener("blur", function(){
+  input.style.background = "aquamarine";
 });
